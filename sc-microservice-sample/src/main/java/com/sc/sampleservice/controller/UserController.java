31a167adafd9446ad0665f97e0e81e9f0b5eb02b
Administrator <Administrator@heige-PC>
2019-11-19 13:27:20 +0800

    增加一个/add_users，大请求量数据的测试方法

--- sc-microservice-sample/src/main/java/com/sc/sampleservice/controller/UserController.java
+++ sc-microservice-sample/src/main/java/com/sc/sampleservice/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +58,12 @@ public class UserController {
 			user.setId(new Long(i));
 			users.add(user);
 		}
+		return users;
+	}
+	
+	@RequestMapping(value = "/add_users", method = RequestMethod.POST, consumes="application/json;charset=UTF-8",produces = "application/json;charset=UTF-8")
+	public List<User> addUsers(@RequestBody List<User> users){
+		logger.info("param users row num:"+(users==null?0:users.size()));
 		return users;
 	}
 	
