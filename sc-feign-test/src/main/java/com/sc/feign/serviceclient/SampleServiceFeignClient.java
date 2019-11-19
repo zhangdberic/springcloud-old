b77214ef21fee4329894bd864b73ad31fb7a0d4c
Administrator <Administrator@heige-PC>
2019-11-19 14:09:54 +0800

    feign代码规则

--- sc-feign-test/src/main/java/com/sc/feign/serviceclient/SampleServiceFeignClient.java
+++ sc-feign-test/src/main/java/com/sc/feign/serviceclient/SampleServiceFeignClient.java
@@ -3,7 +3,6 @@ package com.sc.feign.serviceclient;
 import java.util.List;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
