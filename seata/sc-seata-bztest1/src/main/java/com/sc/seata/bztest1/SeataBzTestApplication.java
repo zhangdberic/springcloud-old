ada2687e0af318641f128b09de20094892346669
Administrator <Administrator@heige-PC>
2019-11-29 17:32:16 +0800

    修改为eureka

--- seata/sc-seata-bztest1/src/main/java/com/sc/seata/bztest1/SeataBzTestApplication.java
+++ seata/sc-seata-bztest1/src/main/java/com/sc/seata/bztest1/SeataBzTestApplication.java
@@ -3,8 +3,6 @@ package com.sc.seata.bztest1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @org.springframework.cloud.openfeign.EnableFeignClients
 public class SeataBzTestApplication {
