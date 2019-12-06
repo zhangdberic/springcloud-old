7ec18e2f9d6f610c22768b4a61d3b5b6869e1e01
zhangdberic <909933699@qq.com>
2019-12-06 16:26:57 +0800

    修改hystrix+turbine

--- sc-hystrix-turbine/src/main/java/com/sc/hystrix/turbine/TurbineServerApplication.java
+++ sc-hystrix-turbine/src/main/java/com/sc/hystrix/turbine/TurbineServerApplication.java
@@ -3,7 +3,11 @@ package com.sc.hystrix.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-
+/**
+ * Hystrix Dashboard监控的turbine地址：http://192.168.5.78:28031/turbine.stream
+ * @author zhangdb
+ *
+ */
 @SpringBootApplication
 @EnableTurbineStream
 public class TurbineServerApplication {
